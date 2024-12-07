cfe3df01cafc56aabd68b307ff31a104c1ef632a
Md Nayeem Hasan Khan <yomee08@gmail.com>
2024-12-07 15:19:50 -0400

    Implemented head

---
+++ head.js
@@ -0,0 +1,18 @@
+// FUNCTION IMPLEMENTATION
+const head = function(array) {
+  return array[0];
+};
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+  
+//TEST CODE FOR HEAD FUNCTION
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([1]), 1);
+assertEqual(head([]), undefined);
