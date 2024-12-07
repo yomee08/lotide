f5bac95eeaed5b3e9f1a3626877d1160b75850eb
Md Nayeem Hasan Khan <yomee08@gmail.com>
2024-12-07 14:59:25 -0400

    Updated assertEqual

--- assertEqual.js
+++ assertEqual.js
@@ -2,9 +2,9 @@
 
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`✅✅✅ Assertion passed: [actual] === [expected]`);
+    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
   } else {
-    console.log(`🛑🛑🛑 Assertion Failed: [actual] !== [expected]`);
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
