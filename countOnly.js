fc0ae15fbbf99ad05f61ef4a5d713dfc1bf481f2
Md Nayeem Hasan Khan <yomee08@gmail.com>
2024-12-10 01:50:14 -0400

    run the code to see the updated result

--- countOnly.js
+++ countOnly.js
@@ -11,6 +11,15 @@ const assertEqual = function(actual, expected) {
 // itemsToCount: an object specifying what to count
 const countOnly = function(allItems, itemsToCount) {
   const results = {};
+
+  for (const item of allItems) {
+    if (results[item]) {
+      results[item] += 1;
+    } else {
+      results[item] = 1;
+    }
+  }
+
   return results;
 };
 
